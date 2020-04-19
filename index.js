9052bee5df947d6d100bcc8f872718d37d9e31c9
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-04-19 10:07:50 +0530

    create a basic server using node t run html and json data

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const Person = require("./Person.js");
-const person1 = new Person("Sayan", 32);
-const Logger = require("./logger.js");
+// const Person = require("./Person.js");
+// const person1 = new Person("Sayan", 32);
+// const Logger = require("./logger.js");
 
-person1.greeting();
+// person1.greeting();
 
-const logger = new Logger();
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-logger.on("message", (data) => console.log("Called Listener:", data));
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
 
-logger.log("Hello World");
-logger.log("Hey Sayan");
+  if (req.url === "/api/users") {
+    const users = [
+      { name: "sayan", age: 32 },
+      { name: "soumi", age: 31 },
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- logger.js
+++ logger.js
@@ -10,4 +10,11 @@ class Logger extends EventEmitter {
   }
 }
 
-module.exports = Logger;
+// module.exports = Logger;
+
+const logger = new Logger();
+
+logger.on("message", (data) => console.log("Called Listener:", data));
+
+logger.log("Hello World");
+logger.log("Hey Sayan");
